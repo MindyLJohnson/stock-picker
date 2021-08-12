d87001f76a87f4db4cc8f90a18af8918b7d0333c
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-11 23:06:25 -0500

    Finalized stock_picker method

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,13 @@
-def stock_picker(closing_values)
-  closing_values
+def stock_picker(stock_price)
+  stock_price.pop if stock_price.index(stock_price.min) == stock_price.length-1
+  stock_price.shift if stock_price.index(stock_price.max) == 0
+
+  stock_price.delete_if do |price|
+    stock_price.index(price) > stock_price.index(stock_price.max)
+  end
+
+  return stock_price.minmax
+
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,10,1])
