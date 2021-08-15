0b7b168273704af0da9ca4108b99aa00d17f8639
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-15 12:16:51 -0500

    Used nested 'collect' methods to find the largest delta in stock prices

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,14 @@
 def stock_picker(stock_prices)
   buy_sell = []
 
+  #Find largest delta
+  stock_returns = stock_prices.each_with_index.collect do |buy_price, buy_day|
+    stock_prices.each_with_index.collect do |sell_price, sell_day|
+      sell_price - buy_price if buy_day < sell_day
+    end
+  end
 
-  return buy_sell
+  return stock_returns.flatten.compact.max
 
 end
 
