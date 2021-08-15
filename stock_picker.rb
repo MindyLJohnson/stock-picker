044fdf4ea25dd16cc71f6d87863916bbe936a39e
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-15 12:41:05 -0500

    Added logic to extract the buy and sell days for the maximum stock return.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,18 @@
 def stock_picker(stock_prices)
   buy_sell = []
 
-  #Find largest delta
   stock_returns = stock_prices.each_with_index.collect do |buy_price, buy_day|
     stock_prices.each_with_index.collect do |sell_price, sell_day|
       sell_price - buy_price if buy_day < sell_day
     end
   end
 
-  return stock_returns.flatten.compact.max
+  max_return = stock_returns.flatten.compact.max
+  buy_sell << stock_returns.index {|returns| returns.include?(max_return)}
+  buy_sell << stock_returns[buy_sell[0]].index(max_return)
+
+  return buy_sell
 
 end
 
-p stock_picker([17,16,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
